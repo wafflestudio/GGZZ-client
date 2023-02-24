ff9b7f43fcec9106be7efc85314558c7bfb1489d
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-02-25 05:49:28 +0900

    fix home styling and submit modal image check

--- src/components/Send/SubmitModal/SubmitModal.tsx
+++ src/components/Send/SubmitModal/SubmitModal.tsx
@@ -2,11 +2,11 @@ import styles from "./SubmitModal.module.scss";
 import { useLetterFormStore } from "../../../../store/useLetterFormStore";
 
 const SubmitModal = () => {
-  const { text, audio, title, setTitle } = useLetterFormStore((state) => state);
+  const { text, audio, image, title, setTitle } = useLetterFormStore((state) => state);
   return (
     <div className={styles.submit}>
       <div className={styles.description}>쪽지 남기기</div>
-      {!text && !audio ? (
+      {!text && !audio && !image ? (
         <div className={styles.noContent}>
           쪽지를 남기려면 <br /> 내용을 입력하세요
         </div>
@@ -21,6 +21,7 @@ const SubmitModal = () => {
           <div className={styles.status}>
             <div>{text ? "글" : ""}</div>
             <div>{audio ? "음성" : ""}</div>
+            <div>{image ? "사진" : ""}</div>
           </div>
           <div className={styles.youLeft}>을 쪽지에 담았습니다!</div>
           <button
