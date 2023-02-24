651a1007755eff7a176bcea96b3f12558ec7f281
isuk <ihyungsuk@snu.ac.kr>
2023-02-25 03:30:58 +0900

    dummy 데이터 추가, radius에 따라 화면상 아이콘도 확대/축소

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,21 +14,32 @@ const getDistPerLatOrLon = (coordinates: TLLCoordinates, forLatNotLon: boolean) 
 };
 
 const geolocationOptions = {
-  enableHighAccuracy: false,
-  timeout: 10000, // 1 min (1000 ms * 60 sec * 1 minute = 60 000ms)
+  enableHighAccuracy: true,
+  timeout: 1000 * 60, // 1 min (1000 ms * 60 sec * 1 minute = 60 000ms)
   maximumAge: 0, // 24 hour
 };
 
 const Home = () => {
-  // const { coordinates, cancelLocationWatch, error } = useWatchLocation(geolocationOptions);
-  const currentLLCoordinates = useGetCurrentLocation(geolocationOptions);
+  const {
+    coordinates: currentLLCoordinates,
+    cancelLocationWatch,
+    error,
+  } = useWatchLocation(geolocationOptions);
+  // const currentLLCoordinates = useGetCurrentLocation(geolocationOptions);
 
   if (!currentLLCoordinates) return;
   const distPerLat = getDistPerLatOrLon(currentLLCoordinates, true);
   const distPerLon = getDistPerLatOrLon(currentLLCoordinates, false);
 
-  const radius = 1000; // m
-  const dummyLetters = [{ LLCoordinates: { lat: 37.479, lon: 126.9507 } }];
+  const radius = 500; // m
+  const dummyLetters = [
+    { LLCoordinates: { lat: 37.480803, lon: 126.950322 } },
+    { LLCoordinates: { lat: 37.481276, lon: 126.950285 } },
+    { LLCoordinates: { lat: 37.482551, lon: 126.952349 } },
+    { LLCoordinates: { lat: 37.480197, lon: 126.9539 } },
+    { LLCoordinates: { lat: 37.479101, lon: 126.95267 } },
+    { LLCoordinates: { lat: 37.479137, lon: 126.95141 } },
+  ];
   const filteredLetters = dummyLetters.filter(
     (letter) => getDistanceFromLatLonInM(currentLLCoordinates, letter.LLCoordinates) <= radius
   );
@@ -44,19 +55,17 @@ const Home = () => {
   return (
     <div className={styles["home"]}>
       <ul className={styles["map"]}>
-        <li className={styles["current-location"]}>
-          {`${currentLLCoordinates.lat}, ${currentLLCoordinates.lon} ${getDistanceFromLatLonInM(
-            currentLLCoordinates,
-            dummyLetters[0].LLCoordinates
-          )}`}
-        </li>
+        <li
+          className={styles["current-location"]}
+          style={{ width: `${(20 * 1000) / radius}px`, height: `${(20 * 1000) / radius}px` }}
+        ></li>
         {LettersDataforDisplay.map((letter, index) => (
           <li
             className={styles["letter"]}
             key={index}
             style={{
-              transform: `translate(${(letter.XYCoordinates.x / (1000 * 2)) * 100}vh, ${
-                (letter.XYCoordinates.y / (1000 * 2)) * 100
+              transform: `translate(${(letter.XYCoordinates.x / (radius * 2)) * 100}vh, ${
+                (letter.XYCoordinates.y / (radius * 2)) * 100
               }vh)`,
             }}
           >
