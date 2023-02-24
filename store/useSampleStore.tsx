a8bd242b714fcd76e15b91296c72e656054976c8
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-02-24 16:00:38 +0900

    add zustand and store sample

---
+++ store/useSampleStore.tsx
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+
+interface SampleStore {
+  sampleData: number;
+  addData: (addValue: number) => void;
+}
+
+const useSampleStore = create<SampleStore>()((set) => ({
+  sampleData: 0,
+  addData: (addValue) => set((state) => ({ sampleData: state.sampleData + addValue })),
+}));
+
+export default useSampleStore;
+
+// 컴포넌트 밖에서 쓸 때는
+// useSampleStore.getState().sampleData나
+// useSampleStore.getState().addData(5) 같은 방식으로 사용
