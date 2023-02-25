5ddfcea9d00f777447372deb5d2046f9ffb45235
isuk <ihyungsuk@snu.ac.kr>
2023-02-25 14:40:33 +0900

    update routes; 잘못된 주소, 비로그인시 처리 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate, useParams, useSearchParams } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Receive from "./pages/Receive/Receive";
 import Register from "./pages/Register";
 import Send from "./pages/Send/Send";
+
+function InValidateURL() {
+  return <Navigate to="/" />;
+}
+
+const checkLoginnedOrNot = () => {
+  throw new Error();
+};
+
+function LoginAndRedirectPage({ redirectPath }: { redirectPath: string }) {
+  const params = useParams();
+  const paramPath = Object.values(params).join("/");
+  const [searchParams] = useSearchParams();
+  const searchParamsPath = searchParams.toString();
+  return (
+    <Navigate
+      to={`/login?redirect=${redirectPath}${params ? "/" + paramPath : ""}${
+        searchParamsPath ? "?" + searchParamsPath : ""
+      }`}
+    />
+  );
+}
+
+const redirectLoginPageIfNotLoginned = async (page: JSX.Element, redirectPath: string) => {
+  try {
+    // await apiLogin(dummyData); // TODO: api 연결 후 수정
+    checkLoginnedOrNot();
+    return page;
+  } catch (e) {
+    return <LoginAndRedirectPage redirectPath={redirectPath} />;
+  }
+};
+// 사용 예시: <Route path="/send" element={redirectLoginPageIfNotLoginned(<Send />, '/send')} />
 
 function App() {
   return (
@@ -14,6 +47,7 @@ function App() {
       <Route path="/receive" element={<Receive />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="*" element={<InValidateURL />} />
     </Routes>
   );
 }
