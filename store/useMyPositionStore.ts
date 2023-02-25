eb1135648cfca47de34d69dd14b0ac3575aa4fd9
isuk <ihyungsuk@snu.ac.kr>
2023-02-25 16:31:57 +0900

    update direction

--- store/useMyPositionStore.ts
+++ store/useMyPositionStore.ts
@@ -4,13 +4,17 @@ import { create } from "zustand";
 type MyPositionStore = {
   currentCoordinates: null | TLLCoordinates;
   viewCoordinates: null | TLLCoordinates;
+  heading: null | number;
   setCurrentCoordinates: (coords: TLLCoordinates) => void;
   setViewCoordinates: (coords: TLLCoordinates | null) => void;
+  setHeading: (heading: number | null) => void;
 };
 
 export const useMyPositionStore = create<MyPositionStore>()((set) => ({
   currentCoordinates: null,
   viewCoordinates: null,
+  heading: null,
   setCurrentCoordinates: (coords) => set({ currentCoordinates: coords }),
   setViewCoordinates: (coords) => set({ viewCoordinates: coords }),
+  setHeading: (heading: number | null) => set({ heading }),
 }));
