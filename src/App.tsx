c1bd9faea15c5a1dd744bc92adbe086078418688
isuk <ihyungsuk@snu.ac.kr>
2023-05-01 18:24:02 +0900

    Refactor: src/App.tsx 안 쓰는 코드 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Routes, Route, Navigate, useParams, useSearchParams } from "react-router-dom";
-import { apiLogin } from "./lib/hooks/apiHooks";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Receive from "./pages/Receive/Receive";
@@ -11,36 +10,6 @@ import { useIntervalToGetLocation } from "./lib/hooks/locationHooks";
 function InValidateURL() {
   return <Navigate to="/" />;
 }
-
-const checkLoginnedOrNot = () => {
-  throw new Error();
-};
-
-function LoginAndRedirectPage({ redirectPath }: { redirectPath: string }) {
-  const params = useParams();
-  const paramPath = Object.values(params).join("/");
-  const [searchParams] = useSearchParams();
-  const searchParamsPath = searchParams.toString();
-  return (
-    <Navigate
-      to={`/login?redirect=${redirectPath}${params ? "/" + paramPath : ""}${
-        searchParamsPath ? "?" + searchParamsPath : ""
-      }`}
-    />
-  );
-}
-
-const redirectLoginPageIfNotLoginned = async (page: JSX.Element, redirectPath: string) => {
-  try {
-    const dummyData = { username: "121213", password: "1331432" };
-    const res = await apiLogin(dummyData); // TODO: api 연결 후 수정
-    // checkLoginnedOrNot();
-    return page;
-  } catch (e) {
-    return <LoginAndRedirectPage redirectPath={redirectPath} />;
-  }
-};
-// 사용 예시: <Route path="/send" element={redirectLoginPageIfNotLoginned(<Send />, '/send')} />
 
 function App() {
   useIntervalToGetLocation();
