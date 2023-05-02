7555f7c888b092be96001f31f5da035c897712f0
HyungWook <69342392+GoGiants1@users.noreply.github.com>
2023-05-02 18:26:02 +0900

    Feat: google map init (#2)
    * Docs: 리드미 내용 추가
    
    * Feat: Add Google Map Packages
    
    * Chore: gitignore 업데이트
    
    * Refactor: 홈 페이지의 미사용 변수 및 함수 삭제
    
    * Style: 종이 배경 제거
    
    * feat: Init Google Map and marker
    
    * Fix: change endpoint base url
    
    * Fix: useCallback 사용
    
    * Refactor: 홈 화면 레이아웃 수정
    
    * Fix: LatLng 타입의 키 값을 구글 맵의 키로 통일
    
    * FIx: 지도 로딩 과정 수정
    - 내 위치가 확인 되어야 지도 로딩되도록.

--- src/lib/hooks/apiHooks.ts
+++ src/lib/hooks/apiHooks.ts
@@ -1,6 +1,5 @@
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 import { useCallback, useLayoutEffect, useState } from "react";
-import { TLLCoordinates } from "../../types/locationTypes";
 
 const url = (path: string, param?: Record<string, any>): string => {
   const validParamData =
@@ -11,7 +10,7 @@ const url = (path: string, param?: Record<string, any>): string => {
         .map(([key, value]) => [key, String(value)])
     );
   return (
-    "https://52.79.248.196.nip.io" + // TODO: 배포 후 수정
+    "https://nostalgia-api-dev.wafflestudio.com" + // TODO: 배포 후 수정
     path +
     (param ? "?" + new URLSearchParams(validParamData).toString() : "")
   );
@@ -50,13 +49,13 @@ export const apiPostLetter = (postLetterData: {
   text: string;
 }) =>
   axios.post(url("/api/v1/letters"), postLetterData, {
-    withCredentials: true,
+    withCredentials: true
   });
 
 export const apiPutLetter = (id: number, postLetterData: FormData) =>
   axios.put(url(`/api/v1/letters/${id}/source`), postLetterData, {
     withCredentials: true,
-    headers: { "Content-Type": "multipart/form-data" },
+    headers: { "Content-Type": "multipart/form-data" }
   });
 
 export const apiGetLetters = (longitude: number, latitude: number) =>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,144 +1,235 @@
-import { useEffect, useState } from "react";
+import { PropsWithChildren, ReactElement, useCallback, useEffect, useRef, useState } from "react";
 import styles from "./Home.module.scss";
-import { TLLCoordinates } from "../types/locationTypes";
-import { getDistanceFromLatLonInM } from "../lib/lib";
-import Letter from "../components/Home/Letter";
 import { useMyPositionStore } from "../../store/useMyPositionStore";
-import me_icon from "../assets/icon/me.svg";
 import { useNavigate } from "react-router-dom";
 import { useHomeModalStore } from "../../store/useHomeModalStore";
 import { ReceiveContainer } from "../components/Home/Receive/Receive";
-import { LetterResponse } from "../types/letterTypes";
-import { apiGetLetters, useApiData, useApiGetLetters } from "../lib/hooks/apiHooks";
-
-const geolocationOptions = {
-  enableHighAccuracy: true,
-  timeout: 60 * 1000, // 1 min (1000 ms * 60 sec * 1 minute = 60 000ms)
-  maximumAge: 0, // 24 hour
-};
-
-const getDistPerLatOrLon = (coordinates: TLLCoordinates, forLatNotLon: boolean) => {
-  const coordinatesForDistanceRatio = forLatNotLon
-    ? { ...coordinates, lat: coordinates.lat + 0.01 }
-    : { ...coordinates, lon: coordinates.lon + 0.01 };
-
-  return getDistanceFromLatLonInM(coordinates, coordinatesForDistanceRatio) * 100;
-};
-const dummyLetters = [
-  { id: 0, LLCoordinates: { lat: 37.480803, lon: 126.950322 } },
-  { id: 1, LLCoordinates: { lat: 37.481276, lon: 126.950285 } },
-  { id: 2, LLCoordinates: { lat: 37.482551, lon: 126.952349 } },
-  { id: 3, LLCoordinates: { lat: 37.480197, lon: 126.9539 } },
-  { id: 4, LLCoordinates: { lat: 37.479101, lon: 126.95267 } },
-  { id: 5, LLCoordinates: { lat: 37.479137, lon: 126.95141 } },
-  { id: 6, LLCoordinates: { lat: 37.482334, lon: 126.953658 } },
-];
-const canOpenRadius = 30;
+import { apiGetLetters, useApiData } from "../lib/hooks/apiHooks";
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import React from "react";
+import { TypeEqualityComparator, createCustomEqual, deepEqual } from "fast-equals";
+import { isLatLngLiteral } from "@googlemaps/typescript-guards";
 
 const Home = () => {
-  const [radius, setRadius] = useState<number>(400);
-  /*  const [letters, setLetters] = useState<
-    { id: number; title: string; summary: string; longitude: number; latitude: number }[]
-  >([]);
- */
-  const heading = useMyPositionStore((state) => state.heading); // useWatchLocation(geolocationOptions);
+  const modalLetter = useHomeModalStore((state) => state.letter);
   const myPosition = useMyPositionStore((state) => state.currentCoordinates);
   const viewPosition = useMyPositionStore((state) => state.viewCoordinates);
   const setViewPosition = useMyPositionStore((state) => state.setViewCoordinates);
   const navigate = useNavigate();
 
-  // 형석: Api 사용
-
-  const currentLLCoordinates = () => {
+  const currentLLCoordinates = useCallback(() => {
     if (viewPosition) return viewPosition;
-    return myPosition ? myPosition : { lat: -37.4780396, lon: -126.945793 };
-  };
+    if (myPosition) {
+      setCenter(myPosition);
+      return myPosition;
+    }
+    return null;
+  }, [myPosition, viewPosition]);
 
   const letters = useApiData<
     { id: number; title: string; summary: string; longitude: number; latitude: number }[]
   >(
-    () => apiGetLetters(currentLLCoordinates().lon ?? 0, currentLLCoordinates().lat ?? 0),
+    () => {
+      const currentLL = currentLLCoordinates();
+      if (!currentLL) return Promise.resolve([]);
+      const { lat, lng } = currentLL;
+      return apiGetLetters(lat, lng);
+    },
     [],
-    [myPosition]
+    [myPosition, viewPosition]
   );
+  const [clicks, setClicks] = React.useState<google.maps.LatLng[]>([]);
+  const [zoom, setZoom] = React.useState(15); // initial zoom
+  const [center, setCenter] = React.useState<google.maps.LatLngLiteral | null>(null);
 
-  const modalLetter = useHomeModalStore((state) => state.letter);
+  const render = useCallback(
+    (status: Status): ReactElement => {
+      if (status === Status.LOADING || center === null) return <h3>{status} ..</h3>;
+      if (status === Status.FAILURE) return <h3>{status} ...</h3>;
+      else {
+        return renderMap();
+      }
+    },
+    [center]
+  );
+  const onClick = useCallback((e: google.maps.MapMouseEvent) => {
+    // avoid directly mutating state
+    if (!e.latLng) return;
+    setClicks([...clicks, e.latLng]);
+  }, []);
 
-  const distPerLat = getDistPerLatOrLon(currentLLCoordinates(), true);
-  const distPerLon = getDistPerLatOrLon(currentLLCoordinates(), false);
+  const onIdle = useCallback((m: google.maps.Map) => {
+    const bounds = m.getBounds();
+    if (!bounds) return;
+    const newCenter = m.getCenter()?.toJSON();
+    if (!newCenter) return;
+    setZoom(m.getZoom() ?? 10);
+    setCenter(newCenter);
+    setViewPosition(newCenter);
+  }, []);
+
+  const renderMap = useCallback((): ReactElement => {
+    return (
+      <Map center={center} onClick={onClick} onIdle={onIdle} zoom={zoom} className={styles["map"]}>
+        {/* TODO: 서버가 내려가 있어서 자세한 테스트는 진행 못함. */}
+        {letters.map((letter) => (
+          <Marker key={letter.id} position={{ lat: letter.latitude, lng: letter.longitude }} />
+        ))}
+        {/* 현재는 클릭하면 마커 생성되게 해둠. */}
+        {clicks.map((latLng, i) => (
+          <Marker key={i} position={latLng} />
+        ))}
+      </Map>
+    );
+  }, [center, onClick, onIdle, zoom, letters, clicks]);
 
   return (
     <div className={styles["home"]}>
-      {!currentLLCoordinates() ? (
-        <div />
-      ) : (
-        <>
-          <ul className={styles["map"]}>
-            <li
-              key="me"
-              className={styles["current-location"]}
-              style={{
-                width: `${(20 * 800) / radius}px`,
-                height: `${(20 * 800) / radius}px`,
-                fontSize: `${(15 * 800) / radius}px`,
-                transform: `rotate(${!heading || isNaN(heading) ? 0 : heading}deg)`,
-              }}
-            >
-              <img
-                className={`${viewPosition ? styles.invisible : styles.visibile}`}
-                src={me_icon}
-              />
-            </li>
-            {letters.map((letter) => (
-              <Letter key={letter.id} letter={letter} radius={radius} />
-            ))}
-          </ul>
-          <ul className={styles["zoom-buttons"]}>
-            <li className={styles["zoom-button"]}>
-              <button
-                onClick={() => {
-                  setRadius((prev) => (prev - 200 >= 200 ? prev - 200 : 200));
-                }}
-              >
-                가까이
-              </button>
-            </li>
-            <li className={styles["rad"]}>{radius}m</li>
-            <li className={styles["zoom-button"]}>
-              <button
-                onClick={() => {
-                  setRadius((prev) => (prev + 200 <= 1400 ? prev + 200 : 1400));
-                }}
-              >
-                멀리
-              </button>
-            </li>
-            <li className={styles["zoom-button"]}>
-              {viewPosition && (
-                <button
-                  onClick={() => {
-                    setViewPosition(null);
-                  }}
-                >
-                  현위치로
-                </button>
-              )}
-            </li>
-          </ul>
-          <button
-            className={styles["new"]}
-            onClick={() => {
-              setViewPosition(null);
-              navigate("./send");
-            }}
-          >
-            새 편지 남기기
-          </button>
-        </>
-      )}
+      <>
+        <Wrapper apiKey={import.meta.env.VITE_GOOGLE_MAP_API_KEY} render={render} />
+        <button
+          className={styles["new"]}
+          onClick={() => {
+            setViewPosition(null);
+            navigate("./send");
+          }}
+        >
+          새 편지 쓰기
+        </button>
+        <button
+          className={styles["my-position-btn"]}
+          onClick={() => {
+            setViewPosition(null);
+            setCenter(myPosition);
+          }}
+        >
+          현재 위치
+        </button>
+      </>
       {modalLetter && <ReceiveContainer />}
     </div>
   );
 };
+
+interface MapProps extends PropsWithChildren<google.maps.MapOptions> {
+  className: string;
+  onClick?: (e: google.maps.MapMouseEvent) => void;
+  onIdle?: (map: google.maps.Map) => void;
+}
+
+const Map: React.FC<MapProps> = ({ onClick, onIdle, children, className, ...options }) => {
+  const ref = React.useRef<HTMLDivElement>(null);
+  const [map, setMap] = React.useState<google.maps.Map>();
+
+  React.useEffect(() => {
+    if (ref.current && !map) {
+      setMap(new window.google.maps.Map(ref.current, {}));
+    }
+  }, [ref, map]);
+
+  // because React does not do deep comparisons, a custom hook is used
+  // see discussion in https://github.com/googlemaps/js-samples/issues/946
+  useDeepCompareEffectForMaps(() => {
+    if (map) {
+      map.setOptions(options);
+    }
+  }, [map, options]);
+
+  React.useEffect(() => {
+    if (map) {
+      ["click", "idle"].forEach((eventName) => google.maps.event.clearListeners(map, eventName));
+
+      if (onClick) {
+        map.addListener("click", onClick);
+      }
+
+      if (onIdle) {
+        map.addListener("idle", () => onIdle(map));
+      }
+    }
+  }, [map, onClick, onIdle]);
+
+  return (
+    <>
+      <div ref={ref} className={className} />
+      {React.Children.map(children, (child) => {
+        if (React.isValidElement(child)) {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          return React.cloneElement(child, { map });
+        }
+      })}
+    </>
+  );
+};
+
+const Marker: React.FC<google.maps.MarkerOptions> = (options) => {
+  const [marker, setMarker] = React.useState<google.maps.Marker>();
+
+  React.useEffect(() => {
+    if (!marker) {
+      setMarker(new google.maps.Marker());
+    }
+
+    // remove marker from map on unmount
+    return () => {
+      if (marker) {
+        marker.setMap(null);
+      }
+    };
+  }, [marker]);
+
+  React.useEffect(() => {
+    if (marker) {
+      marker.setOptions(options);
+    }
+  }, [marker, options]);
+
+  return null;
+};
+
+type LatLngObject = google.maps.LatLng | google.maps.LatLngLiteral;
+
+const customCompare: TypeEqualityComparator<LatLngObject, undefined> = (a: any, b: any) => {
+  if (
+    isLatLngLiteral(a) ||
+    isLatLngLiteral(b) ||
+    a instanceof google.maps.LatLng ||
+    b instanceof google.maps.LatLng
+  ) {
+    if (isLatLngLiteral(a)) {
+      a = new google.maps.LatLng(a);
+    }
+    if (isLatLngLiteral(b)) {
+      b = new google.maps.LatLng(b);
+    }
+    return a.equals(b);
+  }
+
+  // TODO extend to other types
+
+  // use fast-equals for other objects
+  return deepEqual(a, b);
+};
+
+const deepCompareEqualsForMaps = createCustomEqual({
+  createCustomConfig: () => ({
+    areObjectsEqual: customCompare,
+  }),
+});
+
+function useDeepCompareMemoize(value: any) {
+  const ref = React.useRef();
+
+  if (!deepCompareEqualsForMaps(value, ref.current)) {
+    ref.current = value;
+  }
+
+  return ref.current;
+}
+
+function useDeepCompareEffectForMaps(callback: React.EffectCallback, dependencies: unknown[]) {
+  React.useEffect(callback, dependencies.map(useDeepCompareMemoize));
+}
 
 export default Home;
