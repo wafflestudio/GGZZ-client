950dd5729eeda0c058ddd3bf0f2f114e002cb15b
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-05-09 14:56:29 +0900

    Feat: Send 페이지 컴포넌트 생성(기능 없고 디자인만)

--- src/pages/Send/Send.tsx
+++ src/pages/Send/Send.tsx
@@ -1,12 +1,69 @@
 import styles from "./Send.module.scss";
-import { useState } from "react";
+import mic_icon from "../../assets/icon/Send/VoiceSection/mic.svg";
 
-import { useNavigate } from "react-router-dom";
+const LocationSection = () => {
+  return (
+    <div className={styles["locationSection"]}>
+      <div className={styles["label"]}>위치 등록</div>
+      <div className={styles["chips"]}>
+        <div className={styles["locationChip"]}>Hyundai Department Store</div>
+      </div>
+      <div className={styles["map"]}>
+        <div className={styles["fakeMap"]} />
+      </div>
+    </div>
+  );
+};
+
+const TextSection = () => {
+  return (
+    <div className={styles["textSection"]}>
+      <textarea placeholder="끄적끄적..." />
+    </div>
+  );
+};
+
+const ImageSection = () => {
+  return (
+    <div className={styles["imageSection"]}>
+      <div className={styles["imageSlide"]}>
+        <div className={styles["fakeImage"]} />
+        <div className={styles["fakeImage"]} />
+        <div className={styles["fakeImage"]} />
+        <div className={styles["fakeImage"]} />
+        <div className={styles["fakeImage"]} />
+      </div>
+    </div>
+  );
+};
+
+const VoiceSection = () => {
+  return (
+    <div className={styles["voiceSection"]}>
+      <button className={styles["recordButton"]}>
+        <img src={mic_icon} />
+      </button>
+      <div className={styles["playerWrapper"]}></div>
+    </div>
+  );
+};
 
 const Send = () => {
-  const navigate = useNavigate();
-
-  return <div className={styles.send}></div>;
+  return (
+    <div className={styles["send"]}>
+      <header className={styles["header"]}>
+        <div className={styles["title"]}>끄적이기</div>
+        <button className={styles["pasteButton"]}>붙이기</button>
+      </header>
+      <LocationSection />
+      <TextSection />
+      <ImageSection />
+      <VoiceSection />
+      <div className={styles["submit"]}>
+        <button className={styles["mainPasteButton"]}>붙이기</button>
+      </div>
+    </div>
+  );
 };
 
 export default Send;
