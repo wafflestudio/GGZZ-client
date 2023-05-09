89d3e58bb503b47f29ffc236e2f96b123cb291e5
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-05-09 15:19:47 +0900

    Feat: Send 페이지의 입력 로직 추가 (이미지 제외)

--- src/pages/Send/Send.tsx
+++ src/pages/Send/Send.tsx
@@ -1,5 +1,8 @@
 import styles from "./Send.module.scss";
 import mic_icon from "../../assets/icon/Send/VoiceSection/mic.svg";
+import { useLetterFormStore } from "../../store/useLetterFormStore";
+import { useReactMediaRecorder } from "react-media-recorder";
+import { useEffect } from "react";
 
 const LocationSection = () => {
   return (
@@ -16,9 +19,16 @@ const LocationSection = () => {
 };
 
 const TextSection = () => {
+  const text = useLetterFormStore((state) => state.text);
+  const setText = useLetterFormStore((state) => state.setText);
+
   return (
     <div className={styles["textSection"]}>
-      <textarea placeholder="끄적끄적..." />
+      <textarea
+        placeholder="끄적끄적..."
+        value={text ? text : ""}
+        onChange={(e) => setText(e.target.value)}
+      />
     </div>
   );
 };
@@ -38,12 +48,67 @@ const ImageSection = () => {
 };
 
 const VoiceSection = () => {
+  const audio = useLetterFormStore((state) => state.audio);
+  const setAudio = useLetterFormStore((state) => state.setAudio);
+  const { status, startRecording, stopRecording, mediaBlobUrl, clearBlobUrl } =
+    useReactMediaRecorder({
+      audio: true,
+      video: false,
+    });
+
+  //녹음 정지 시 자동 저장
+  useEffect(() => {
+    if (status === "stopped") {
+      if (mediaBlobUrl)
+        fetch(mediaBlobUrl)
+          .then((res) => res.blob())
+          .then((audioBlob) => {
+            setAudio(audioBlob);
+          });
+    }
+  }, [status]);
+
   return (
     <div className={styles["voiceSection"]}>
-      <button className={styles["recordButton"]}>
+      <button
+        className={`${styles["recordButton"]} ${
+          (status === "recording" || audio) && styles["recording"]
+        } }`}
+        onClick={() => {
+          if (status === "idle") {
+            startRecording();
+          }
+          if (status === "stopped") {
+            if (audio) {
+              clearBlobUrl();
+              setAudio(null);
+              startRecording();
+            }
+          }
+          if (status === "recording") {
+            stopRecording();
+          }
+        }}
+      >
         <img src={mic_icon} />
       </button>
-      <div className={styles["playerWrapper"]}></div>
+      <div className={styles["playerWrapper"]}>
+        {<audio src={audio ? URL.createObjectURL(audio) : ""} controls />}
+      </div>
+    </div>
+  );
+};
+
+const SubmitSection = () => {
+  const { text, audio, image } = useLetterFormStore((state) => state);
+
+  return (
+    <div className={styles["submitSection"]}>
+      <button
+        className={`${styles["mainPasteButton"]} ${(text || audio || image) && styles["active"]}`}
+      >
+        붙이기
+      </button>
     </div>
   );
 };
@@ -59,9 +124,7 @@ const Send = () => {
       <TextSection />
       <ImageSection />
       <VoiceSection />
-      <div className={styles["submit"]}>
-        <button className={styles["mainPasteButton"]}>붙이기</button>
-      </div>
+      <SubmitSection />
     </div>
   );
 };
