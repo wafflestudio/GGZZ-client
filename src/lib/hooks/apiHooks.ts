0d8be3eb6f5904a9d14abc9b4bdf8f9df62d9acd
isuk <ihyungsuk@snu.ac.kr>
2023-07-27 18:09:41 +0900

    FIX : api 주소 수정

--- src/lib/hooks/apiHooks.ts
+++ src/lib/hooks/apiHooks.ts
@@ -10,7 +10,7 @@ const url = (path: string, param?: Record<string, any>): string => {
         .map(([key, value]) => [key, String(value)])
     );
   return (
-    "https://nostalgia-api-dev.wafflestudio.com" + // TODO: 배포 후 수정
+    "https://ggzz-api-dev.wafflestudio.com" + // TODO: 배포 후 수정
     path +
     (param ? "?" + new URLSearchParams(validParamData).toString() : "")
   );
