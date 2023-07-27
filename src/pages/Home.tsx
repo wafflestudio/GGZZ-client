a844a745d5a92d653fcd1fd51b741364ab23c994
andhl204 <swysylvia204@gmail.com>
2023-07-27 19:47:28 +0900

    Feat/map html marker (#4)
    * Feat: custom html label
    
    * Feat: custom html marker settings
    
    * Feat: add click event to html marker
    
    * Design: custom marker css
    
    * Style: separate Home codes
    
    * Design: when no image
    
    * build: 버전 오류 해결
    
    * Fix: warning 해결
    
    * Design: 아이폰에서 주소창 때문에 버튼 안보이는 경우 수정
    
    * Remove: 이전에 쓰던 새 편지, 현 위치 버튼 제거
    
    * Feat: 홈 화면의 생성 버튼
    
    * Remove: 로그인 검증하는 로직과 API 관련 함수들
    
    * Refactor: Send 페이지 안 쓰는 요소들 제거 및 deprecated로 이동
    
    * Feat: Send 페이지 컴포넌트 생성(기능 없고 디자인만)
    
    * Feat: Send 페이지의 입력 로직 추가 (이미지 제외)
    
    * Feat: Send에 구글맵 띄우기
    
    * Refactor: Send 페이지의 지도를 <Map> 컴포넌트로 대체
    
    * Feat: image section 추가
    
    * Fix: SubmitSection padding 오류
    
    * Fix: chips 가로로 스크롤 가능하게
    
    * Refact: section 태그 사용하게 변경
    
    * Refact: Send 페이지의 Section 컴포넌트들 별도 파일로 분리
    
    * Comment: 해결된 ASK 삭제
    
    * Comment: ImageSection todo 추가
    
    * Fix: react-media-recorder 라이브러리와 Next 간의 충돌 해결하기 위해 라이브러리 react-media-recorder-2로 교체
    
    * Fix: 구글 맵 로딩 시 UI waterfall 현상 해결
    
    * Fix: image url 관리 방식 변경
    
    * Feat: post mesage를 위한 가짜 코드 추가
    
    * FIX : api 주소 수정
    
    * 임시 오류 처리)
    
    * UPDATE : clicks => letters로 변경
    
    * 줄바꿈 수정
    
    * UPDATE : clicks => letters 수정
    
    * TODO 주석 추가
    
    * FIX : build 시 발생하는 warnings 제거
    
    * TODO 주석 추가
    
    * FIX : conflict resolve 과정 오류 해결
    
    ---------
    
    Co-authored-by: GoGiants1 <chooi221@snu.ac.kr>
    Co-authored-by: designdefined.by@gmail.com <designdefined.by@gmail.com>
    Co-authored-by: isuk <ihyungsuk@snu.ac.kr>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,28 @@
-import {
-  PropsWithChildren,
-  ReactElement,
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-  useLayoutEffect,
-} from "react";
+import { useCallback, useState, useLayoutEffect } from "react";
 import styles from "./Home.module.scss";
-import { TLLCoordinates } from "../lib/types/locationTypes";
-import { getDistanceFromLatLngInM } from "../lib/lib";
-import Letter from "../components/Home/Letter";
 import NavigationTab from "../components/Home/NavigationTab";
-import { useMyPositionStore } from "../store/useMyPositionStore";
-import me_icon from "../assets/icon/me.svg";
 import { useNavigate } from "react-router-dom";
-import { useHomeModalStore } from "../store/useHomeModalStore";
 import { ReceiveContainer } from "../components/Home/Receive/Receive";
 import SplashScreen from "../components/Home/SplashScreen";
 import { apiGetLetters, useApiData } from "../lib/hooks/apiHooks";
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import { useHomeModalStore } from "../store/useHomeModalStore";
+import { useMyPositionStore } from "../store/useMyPositionStore";
+import Map from "../components/Home/Map/Map";
+import sendIcon from "../assets/icon/Home/SendButton/send.svg";
 import React from "react";
 import { TypeEqualityComparator, createCustomEqual, deepEqual } from "fast-equals";
 import { isLatLngLiteral } from "@googlemaps/typescript-guards";
 import useEtcStore from "../store/useEtcStore";
 
 const Home = () => {
+  const [clicks, setClicks] = useState<google.maps.LatLng[]>([]);
+  const [zoom, setZoom] = useState(15); // initial zoom
+  const [center, setCenter] = useState<google.maps.LatLngLiteral | null>({
+    lat: 37.46474,
+    lng: 126.954547,
+  });
+
   const modalLetter = useHomeModalStore((state) => state.letter);
   const myPosition = useMyPositionStore((state) => state.currentCoordinates);
   const viewPosition = useMyPositionStore((state) => state.viewCoordinates);
@@ -42,7 +39,15 @@ const Home = () => {
   }, [myPosition, viewPosition]);
 
   const letters = useApiData<
-    { id: number; title: string; summary: string; longitude: number; latitude: number }[]
+    {
+      id: number;
+      createdAt: string;
+      createdBy: string;
+      title: string;
+      summary: string;
+      longitude: number;
+      latitude: number;
+    }[]
   >(
     () => {
       const currentLL = currentLLCoordinates();
@@ -53,20 +58,15 @@ const Home = () => {
     [],
     [myPosition, viewPosition]
   );
-  const [clicks, setClicks] = React.useState<google.maps.LatLng[]>([]);
-  const [zoom, setZoom] = React.useState(15); // initial zoom
-  const [center, setCenter] = React.useState<google.maps.LatLngLiteral | null>(null);
 
   const render = useCallback(
-    (status: Status): ReactElement => {
-      if (status === Status.LOADING || center === null) return <h3>{status} ..</h3>;
+    (status: Status) => {
       if (status === Status.FAILURE) return <h3>{status} ...</h3>;
-      else {
-        return renderMap();
-      }
+      return <h3>{status} ..</h3>;
     },
     [center]
   );
+
   const onClick = useCallback((e: google.maps.MapMouseEvent) => {
     // avoid directly mutating state
     if (!e.latLng) return;
@@ -83,21 +83,6 @@ const Home = () => {
     setViewPosition(newCenter);
   }, []);
 
-  const renderMap = useCallback((): ReactElement => {
-    return (
-      <Map center={center} onClick={onClick} onIdle={onIdle} zoom={zoom} className={styles["map"]}>
-        {/* TODO: 서버가 내려가 있어서 자세한 테스트는 진행 못함. */}
-        {letters.map((letter) => (
-          <Marker key={letter.id} position={{ lat: letter.latitude, lng: letter.longitude }} />
-        ))}
-        {/* 현재는 클릭하면 마커 생성되게 해둠. */}
-        {clicks.map((latLng, i) => (
-          <Marker key={i} position={latLng} />
-        ))}
-      </Map>
-    );
-  }, [center, onClick, onIdle, zoom, letters, clicks]);
-
   // show splash screen for first 4 seconds
   // isLoading === true: splash screen
   const isLoading = useEtcStore((state) => state.isLoading);
@@ -108,7 +93,7 @@ const Home = () => {
         check();
       }, 4000);
   }, [check]);
-
+      
   return (
     <>
       {isLoading && (
@@ -119,151 +104,34 @@ const Home = () => {
       <div className={styles["home"]}>
         <NavigationTab />
         <>
-          <Wrapper apiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY || ""} render={render} />
-          <button
-            className={styles["new"]}
-            onClick={() => {
-              setViewPosition(null);
-              navigate("./send");
-            }}
+          <Wrapper
+            apiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY || ""}
+            version="beta"
+            libraries={["marker"]}
+            render={render}
           >
-            새 편지 쓰기
-          </button>
-          <button
-            className={styles["my-position-btn"]}
-            onClick={() => {
-              setViewPosition(null);
-              setCenter(myPosition);
-            }}
-          >
-            현재 위치
-          </button>
+            <Map
+              center={center}
+              onClick={onClick}
+              onIdle={onIdle}
+              zoom={zoom}
+              letters={letters}
+              className={styles["map"]}
+            />
+          </Wrapper>
         </>
+        <button
+          className={styles["send"]}
+          onClick={() => {
+            navigate("send");
+          }}
+        >
+          <img src={sendIcon} />
+        </button>
         {modalLetter && <ReceiveContainer />}
       </div>
     </>
   );
 };
-
-interface MapProps extends PropsWithChildren<google.maps.MapOptions> {
-  className: string;
-  onClick?: (e: google.maps.MapMouseEvent) => void;
-  onIdle?: (map: google.maps.Map) => void;
-}
-
-const Map: React.FC<MapProps> = ({ onClick, onIdle, children, className, ...options }) => {
-  const ref = React.useRef<HTMLDivElement>(null);
-  const [map, setMap] = React.useState<google.maps.Map>();
-
-  React.useEffect(() => {
-    if (ref.current && !map) {
-      setMap(new window.google.maps.Map(ref.current, {}));
-    }
-  }, [ref, map]);
-
-  // because React does not do deep comparisons, a custom hook is used
-  // see discussion in https://github.com/googlemaps/js-samples/issues/946
-  useDeepCompareEffectForMaps(() => {
-    if (map) {
-      map.setOptions(options);
-    }
-  }, [map, options]);
-
-  React.useEffect(() => {
-    if (map) {
-      ["click", "idle"].forEach((eventName) => google.maps.event.clearListeners(map, eventName));
-
-      if (onClick) {
-        map.addListener("click", onClick);
-      }
-
-      if (onIdle) {
-        map.addListener("idle", () => onIdle(map));
-      }
-    }
-  }, [map, onClick, onIdle]);
-
-  return (
-    <>
-      <div ref={ref} className={className} />
-      {React.Children.map(children, (child) => {
-        if (React.isValidElement(child)) {
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
-          return React.cloneElement(child, { map });
-        }
-      })}
-    </>
-  );
-};
-
-const Marker: React.FC<google.maps.MarkerOptions> = (options) => {
-  const [marker, setMarker] = React.useState<google.maps.Marker>();
-
-  React.useEffect(() => {
-    if (!marker) {
-      setMarker(new google.maps.Marker());
-    }
-
-    // remove marker from map on unmount
-    return () => {
-      if (marker) {
-        marker.setMap(null);
-      }
-    };
-  }, [marker]);
-
-  React.useEffect(() => {
-    if (marker) {
-      marker.setOptions(options);
-    }
-  }, [marker, options]);
-
-  return null;
-};
-
-type LatLngObject = google.maps.LatLng | google.maps.LatLngLiteral;
-
-const customCompare: TypeEqualityComparator<LatLngObject, undefined> = (a: any, b: any) => {
-  if (
-    isLatLngLiteral(a) ||
-    isLatLngLiteral(b) ||
-    a instanceof google.maps.LatLng ||
-    b instanceof google.maps.LatLng
-  ) {
-    if (isLatLngLiteral(a)) {
-      a = new google.maps.LatLng(a);
-    }
-    if (isLatLngLiteral(b)) {
-      b = new google.maps.LatLng(b);
-    }
-    return a.equals(b);
-  }
-
-  // TODO extend to other types
-
-  // use fast-equals for other objects
-  return deepEqual(a, b);
-};
-
-const deepCompareEqualsForMaps = createCustomEqual({
-  createCustomConfig: () => ({
-    areObjectsEqual: customCompare,
-  }),
-});
-
-function useDeepCompareMemoize(value: any) {
-  const ref = React.useRef();
-
-  if (!deepCompareEqualsForMaps(value, ref.current)) {
-    ref.current = value;
-  }
-
-  return ref.current;
-}
-
-function useDeepCompareEffectForMaps(callback: React.EffectCallback, dependencies: unknown[]) {
-  React.useEffect(callback, dependencies.map(useDeepCompareMemoize));
-}
 
 export default Home;
