f0f1947619accef0a7c0b965b9df3c0cfc66e9f7
isuk <ihyungsuk@snu.ac.kr>
2023-07-27 19:37:16 +0900

    FIX : conflict resolve 과정 오류 해결

--- src/pages/Send/Send.tsx
+++ src/pages/Send/Send.tsx
@@ -1,29 +1,14 @@
 import styles from "./Send.module.scss";
-import { useLayoutEffect, useState } from "react";
-import writing_icon from "../../assets/icon/writing.svg";
-import mic_icon from "../../assets/icon/mic.svg";
-import camera_icon from "../../assets/icon/camera.svg";
-import close_icon from "../../assets/icon/close.svg";
-import submit_icon from "../../assets/icon/corner.svg";
-
-import VoiceModal from "../../components/Send/VoiceModal/VoiceModal";
-import TextModal from "../../components/Send/TextModal/TextModal";
-import { useNavigate } from "react-router-dom";
-import SubmitModal from "../../components/Send/SubmitModal/SubmitModal";
-import ImageModal from "../../components/Send/ImageModal/ImageModal";
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import LocationSection from "../../components/Send/LocationSection/LocationSection";
 import TextSection from "../../components/Send/TextSection/TextSection";
 import ImageSection from "../../components/Send/ImageSection/ImageSection";
 import VoiceSection from "../../components/Send/VoiceSection/VoiceSection";
 import SubmitSection from "../../components/Send/SubmitSection/SubmitSection";
+import { useNavigate } from "react-router-dom";
 
 const Send = () => {
-  const [inputModal, setInputModal] = useState<"none" | "writing" | "voice" | "image" | "submit">(
-    "none"
-  );
   const navigate = useNavigate();
-
   useLayoutEffect(() => {
     const token = sessionStorage.getItem("accessToken");
     if (!token) {
